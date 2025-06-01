0b784b06d740f45b4ebd577491c7417ec04fbd0f
surya223532 <jayasekali223532@gmail.com>
2025-06-01 22:02:48 +0800

    perbaikan login cssnya

--- login.php
+++ login.php
@@ -39,22 +39,30 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <title>Login SIMAN</title>
+    <link rel="stylesheet" href="/SIMAN/assets/login.css">
 </head>
 <body>
-    <h2>Login SIMAN</h2>
-    
-    <form action="login.php" method="POST">
-        <label>Email:</label><br>
-        <input type="text" name="email" required><br><br>
+    <div class="login-container">
+        <img src="/SIMAN/assets/img/logo.png" alt="Logo Institusi" class="logo">
+        <h1>SISTEM MANAJEMEN INVENTARIS KAMPUS ITH</h1>
+        
+        <form action="login.php" method="POST">
+            <div class="input-group">
+                <label for="email">Username</label>
+                <input type="text" id="email" name="email" required>
+            </div>
+            
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            
+            <button type="submit" name="login">Masuk</button>
+        </form>
 
-        <label>Password:</label><br>
-        <input type="password" name="password" required><br><br>
-
-        <button type="submit" name="login">Login</button>
-    </form>
-
-    <?php if (isset($_GET['error'])): ?>
-        <p style="color:red;"><?php echo htmlspecialchars($_GET['error']); ?></p>
-    <?php endif; ?>
+        <?php if (isset($_GET['error'])): ?>
+            <p class="error-message"><?php echo htmlspecialchars($_GET['error']); ?></p>
+        <?php endif; ?>
+    </div>
 </body>
 </html>
